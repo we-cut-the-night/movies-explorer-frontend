d6fcf42f0a1fe7e600488c4210d7c340ce44c80f
esman artem <jesman@yandex.ru>
2022-09-09 04:41:19 +0200

    Constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,5 @@
-export const BASE_URL = 'https://api.nomoreparties.co';
 export const BASE_URL_MOVIES = 'https://api.nomoreparties.co/beatfilm-movies';
-export const BASE_URL_MAIN = 'http://localhost:3000';
+export const BASE_URL_MAIN = 'https://api.cinemaclub.nomoredomains.sbs';
 export const PAGE_CAPACITY_MIN = 769;
 export const PAGE_CAPACITY_MID = 997;
 export const PAGE_CAPACITY_MAX = 1285;
